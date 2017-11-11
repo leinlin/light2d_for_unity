f0dd900121c0d58e16677cca7ac55e394c663c60
ElPsyCongree <345036769@qq.com>
2017-11-11 18:08:28 +0800

    commit

--- Assets/Script/CSG.cs
+++ Assets/Script/CSG.cs
@@ -41,19 +41,8 @@ using System.IO;
 using UnityEngine;
 
 public class CSG : MonoBehaviour {
-    struct Result {
-        public Result(float sd, float emissive) {
-            this.sd = sd;
-            this.emissive = emissive;
-        }
-
-        public float sd;
-        public float emissive; 
-    }
 
     #region member
-    private int m_verticesCount = 4;
-    private Material spriteMaterial;
     private const int W = 512;
     private const int H = 512;
 
@@ -63,11 +52,27 @@ public class CSG : MonoBehaviour {
     private const float EPSILON = 1e-6f;
     private const float TWO_PI = 6.28318530718f;
     private const float N = 64;
+
+    //其实这里可以搞个对象池，优化下
+    struct Result {
+        public Result(float sd, float emissive) {
+            this.sd = sd;
+            this.emissive = emissive;
+        }
+
+        public float sd;
+        public float emissive;
+    }
     #endregion
 
     // Use this for initialization
     void Start() {
-        initSprite();
+        Shader s = Shader.Find("Unlit/Texture");
+        Material spriteMaterial = new Material(s);
+        Texture2D t = GenLightPic();
+        spriteMaterial.mainTexture = t;
+
+        RenderUtil.ShowAllSprite(spriteMaterial, gameObject);
     }
 
     #region light2d
@@ -146,60 +151,6 @@ public class CSG : MonoBehaviour {
     float circleSDF(float x, float y, float cx, float cy, float r) {
         float ux = x - cx, uy = y - cy;
         return Mathf.Sqrt(ux * ux + uy * uy) - r;
-    }
-    #endregion
-
-    #region show
-    //根据宽高生成对应的面//  
-    private void initSprite() {
-        Shader s = Shader.Find("Unlit/Texture");
-        spriteMaterial = new Material(s);
-        Texture2D t = GenLightPic();
-        spriteMaterial.mainTexture = t;
-
-        //获取图片的像素宽高//  
-        int pixelHeight = t.height;
-        int pixelWidth = t.width;
-
-        Debug.Log("pixeW:" + pixelWidth + ",pixeH:" + pixelHeight);
-
-        //得到MeshFilter对象//  
-        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-        MeshRenderer meshRenderer = null;
-        if (meshFilter == null) {
-            //为null时，自动添加//  
-            meshFilter = gameObject.AddComponent<MeshFilter>();
-            meshRenderer = gameObject.AddComponent<MeshRenderer>();
-            meshRenderer.sharedMaterial = spriteMaterial;
-        }
-        //得到对应的网格对象//  
-        Mesh mesh = meshFilter.mesh;
-        //三角形顶点的坐标数组//  
-        Vector3[] vertices = new Vector3[m_verticesCount];
-        //三角形顶点数组//  
-        int[] triangles = new int[m_verticesCount * 3];
-
-
-        float glWidth = pixelWidth / 2;
-        float glHeight = pixelHeight / 2;
-        //以当前对象的中心坐标为标准//  
-        vertices[0] = new Vector3(-glWidth, -glHeight, 0);
-        vertices[1] = new Vector3(-glWidth, glHeight, 0);
-        vertices[2] = new Vector3(glWidth, -glHeight, 0);
-        vertices[3] = new Vector3(glWidth, glHeight, 0);
-
-        mesh.vertices = vertices;
-        //绑定顶点顺序//  
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
-        triangles[3] = 1;
-        triangles[4] = 3;
-        triangles[5] = 2;
-
-        mesh.triangles = triangles;
-
-        mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 0), new Vector2(1, 1) };
     }
     #endregion
 }
---
+++ Assets/Script/Util/RenderUtil.cs
@@ -0,0 +1,86 @@
+﻿/*
+               #########                       
+              ############                     
+              #############                    
+             ##  ###########                   
+            ###  ###### #####                  
+            ### #######   ####                 
+           ###  ########## ####                
+          ####  ########### ####               
+         ####   ###########  #####             
+        #####   ### ########   #####           
+       #####   ###   ########   ######         
+      ######   ###  ###########   ######       
+     ######   #### ##############  ######      
+    #######  #####################  ######     
+    #######  ######################  ######    
+   #######  ###### #################  ######   
+   #######  ###### ###### #########   ######   
+   #######    ##  ######   ######     ######   
+   #######        ######    #####     #####    
+    ######        #####     #####     ####     
+     #####        ####      #####     ###      
+      #####       ###        ###      #        
+        ###       ###        ###               
+         ##       ###        ###               
+__________#_______####_______####______________
+
+                我们的未来没有BUG              
+* ==============================================================================
+* Filename: RenderUtil.cs
+* Created:  2017/11/11 15:52:52
+* Author:   To Hard The Mind
+* Purpose:  
+* ==============================================================================
+*/
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RenderUtil {
+    public static void ShowAllSprite(Material spriteMaterial, GameObject gameObject) {
+        Texture t = spriteMaterial.mainTexture;
+
+        //获取图片的像素宽高//  
+        int pixelHeight = t.height;
+        int pixelWidth = t.width;
+
+        //得到MeshFilter对象//  
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = null;
+        if (meshFilter == null) {
+            //为null时，自动添加//  
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = spriteMaterial;
+        }
+        //得到对应的网格对象//  
+        Mesh mesh = meshFilter.mesh;
+        //三角形顶点的坐标数组//  
+        Vector3[] vertices = new Vector3[4];
+        //三角形顶点数组//  
+        int[] triangles = new int[6];
+
+
+        float glWidth = pixelWidth / 2;
+        float glHeight = pixelHeight / 2;
+        //以当前对象的中心坐标为标准//  
+        vertices[0] = new Vector3(-glWidth, -glHeight, 0);
+        vertices[1] = new Vector3(-glWidth, glHeight, 0);
+        vertices[2] = new Vector3(glWidth, -glHeight, 0);
+        vertices[3] = new Vector3(glWidth, glHeight, 0);
+
+        mesh.vertices = vertices;
+        //绑定顶点顺序//  
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 1;
+        triangles[4] = 3;
+        triangles[5] = 2;
+
+        mesh.triangles = triangles;
+
+        mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 0), new Vector2(1, 1) };
+    }
+}
